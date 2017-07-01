0d5e5a7ea2aa2ce3927b73d75151fb9493f7d00a
Anton Rufino <anton.vongola@gmail.com>
2017-07-02 03:03:49 +0800

    sasdfds

--- frontend/src/AppContainer/index.js
+++ frontend/src/AppContainer/index.js
@@ -10,6 +10,39 @@ import Restaurant from './Restaurant';
 import {Switch, Route} from 'react-router-dom';
 
 const AppContainer = (props) => {
+    const dummyMenu = [
+        {
+            foodId: 1,
+            name: "Burger",
+            price: 69,
+            thumbnail: "https://d34nj53l8zkyd3.cloudfront.net/ph/view/2ce8e502a7"
+        },
+        {
+            foodId: 1,
+            name: "Cake",
+            price: 70,
+            thumbnail: "https://d34nj53l8zkyd3.cloudfront.net/ph/view/2ce8e502a7"
+        },
+        {
+            foodId: 1,
+            name: "Spaghetti",
+            price: 71,
+            thumbnail: "https://d34nj53l8zkyd3.cloudfront.net/ph/view/2ce8e502a7"
+        },
+        {
+            foodId: 1,
+            name: "Italian Chicken Ala King",
+            price: 72,
+            thumbnail: "https://d34nj53l8zkyd3.cloudfront.net/ph/view/2ce8e502a7"
+        },
+        {
+            foodId: 1,
+            name: "French Fries",
+            price: 73,
+            thumbnail: "https://d34nj53l8zkyd3.cloudfront.net/ph/view/2ce8e502a7"
+        }
+    ];
+
     const onShowChooser = () => {
         props.showChooser();
     };
@@ -32,7 +65,7 @@ const AppContainer = (props) => {
             <Header user={props.user} />
             <Switch>
                 <Route exact path="/" component={(mProps) => <RestaurantList {...mProps} restaurants={props.restaurants} onShowChooser={onShowChooser} /> } />
-                <Route path="/restaurant/:id" component={(mProps) => <Restaurant {...mProps} restaurant={getRestaurant(mProps)} menu={props.menu || []} />} />
+                <Route path="/restaurant/:id" component={(mProps) => <Restaurant {...mProps} restaurant={getRestaurant(mProps)} menu={props.menu || dummyMenu} />} />
             </Switch>
         </Div>
     );
